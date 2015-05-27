5d102768f79b2e8a9bd583f05087edaec8a35ae6
AjayPoshak <poshakajay@gmail.com>
2015-05-28 00:39:19 +0530

    Iteration01
    Initial Work as Done till 28 May 2015.

---
+++ MyBlog/View/Footer.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap Library Files -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<!-- Customized Style Sheets -->
+	<link rel="stylesheet" type="text/css" href="View/style.css">
+</head>
+<body>
+		<div class="container-fluid">
+	<footer class="footer" style="background-color:BLACK;">
+
+			<p class="text-muted">All views written on this site are entirely personal and are not intended to malign or harm anyone's personal reputation.
+					Usage of contents of this page are subjected to owner's prior permission.
+					<center>&copy;AJAY POSHAK</center></p>
+				
+			</footer>
+		</div>
+</body>
+</html>
---
+++ MyBlog/View/Head.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap Library Files -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<!-- Customized Style Sheets -->
+	<link rel="stylesheet" type="text/css" href="View/style.css">
+	
+</head>
+<body>
+	<div class="container-fluid header-Img-padding">
+	<div class="col-lg-16 col-md-16 col-sm-16 col-xs-16">
+	<div class="page-header" id="header">
+		<img class="img-responsive img-rounded" src="View/banner01.jpg" alt=Banner Image">
+	<nav class="navbar navbar-default navbar-static-top">
+      <ul class="nav navbar-nav">
+	  <li class="active"><a href="#Home">Home</a></li>
+        <li class="#politics"><a href="#polity">Politics</a></li>
+        <li><a href="#tech">Technology</a></li>
+		<li><a href="#pers">Personal</a></li>
+		<li><a href="#misc">Miscellaneous</a></li>
+      </ul>
+	  </nav>
+	  </div>
+    </div>
+  
+
+</div>
+</body>
+</html>
