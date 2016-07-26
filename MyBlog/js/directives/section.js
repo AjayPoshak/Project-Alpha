6ea3136b0b6abed422f2b494c6b759e4aa94b2e7
Ajay Poshak <poshakajay@gmail.com>
2016-07-26 13:13:31 +0530

    updated linking of templates

--- MyBlog/js/directives/section.js
+++ MyBlog/js/directives/section.js
@@ -1,12 +1,13 @@
 //Section.js
+"use strict";
 app.directive('blogHeader', function()
 {
 	console.log("Inside Header directive ...");
 	return{
-		templateUrl : "partials/header_new.html",
+		templateUrl : "partials/header.html",
 		controller : ''
 	};
-	
+
 });
 app.directive('blogFooter', function()
 {
@@ -15,40 +16,5 @@ app.directive('blogFooter', function()
 		templateUrl : "partials/footer.html",
 		controller : ''
 	};
-	
-});
-app.directive("blogMainpage", function(){
-	console.log("Inside Main Page");
-	return{
-		templateUrl : "partials/main-page.html",
-		controller:''
-	}
-});
-app.directive("blogPolitics", function(){
-	console.log("Inside Politics...");
-	return{
-		templateUrl : 'partials/politics.html',
-		controller : ''
-	}
-});
-app.directive("blogTech", function(){
-	console.log("Inside Technology...");
-	return{
-		templateUrl : 'partials/tech.html',
-		controller : ''
-	}
-});
-app.directive("blogPersonal", function(){
-	console.log("Inside Personal...");
-	return{
-		templateUrl : 'partials/personal.html',
-		controller : ''
-	}
-});
-app.directive("blogMisc", function(){
-	console.log("Inside Misc....");
-	return{
-		templateUrl : 'partials/misc.html',
-		controller : ''
-	}
+
 });
