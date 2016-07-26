5136690fd55b8a1ff2dbe684022579fa34717b5a
Ajay Poshak <poshakajay@gmail.com>
2016-07-26 13:12:50 +0530

    Added strict mode

--- MyBlog/js/controllers/LinkController.js
+++ MyBlog/js/controllers/LinkController.js
@@ -1,7 +1,8 @@
+"use strict";
 app.controller('LinkController', function($scope, $http, $rootScope){
 	$rootScope.pageId = localStorage.getItem("pageId");
 	console.log("Inside Link Controller"+$rootScope.pageId);
-	
+
 	$http.get("Contents/content.json")
 		.success(function(data){
 			$scope.content = data;
