98ccee761d0b779501f6469b53b50a06ccb6b20d
Ajay Poshak <poshakajay@gmail.com>
2016-09-25 10:38:55 +0530

    Route updated

--- MyBlog/route.js
+++ MyBlog/route.js
@@ -8,25 +8,38 @@ var index = require('./index.js');
  * request : standard request object
  * response : standard response object
  */
-http.createServer(function(request, response){
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost');
-  var url_parts = url.parse(request.url);
-  console.log(url_parts.pathname);
-  var dbCall = index(response);
+http.createServer(function(request, response) {
+    response.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    var url_parts = url.parse(request.url);
+    console.log(url_parts.pathname);
+    console.log(url_parts);
+    var slashLocation = url_parts.pathname.toString().substring(1).indexOf('/');
+    console.log("slashLocation::::" + slashLocation);
+    var updatedPathName = url_parts.pathname;
+    if (slashLocation !== -1)
+      updatedPathName = url_parts.pathname.substring(0, slashLocation+1);
+    console.log(url_parts.pathname);
 
-  switch(url_parts.pathname){
-    case '/':
-      console.log('Display roots');
-      break;
-    case '/showData':
-      dbCall.selectData();
-      break;
-    default:
-      console.log('404, Page not found');
-      response.statusMessage = "Page Not Found";
-      response.statusCode = 404;
-      response.end('ok');
-  };
+    var dbCall = index(response);
 
-}).listen(8000);
-console.log("Listening at localhost:8000");
+    switch (updatedPathName) {
+        case '/':
+            console.log('Display roots');
+            break;
+        case '/showHomePageData':
+            dbCall.selectData();
+            break;
+        case '/getArticlebyId':
+            var id = url_parts.pathname.toString().substring(slashLocation+2);
+            console.log('getting article by id..........'+id);
+            dbCall.getArticlebyId(id);
+            break;
+        default:
+            console.log('404, Page not found');
+            response.statusMessage = "Page Not Found";
+            response.statusCode = 404;
+            response.end('ok');
+    };
+
+}).listen(8001);
+console.log("Listening at localhost:8001");
