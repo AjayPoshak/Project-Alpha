d3e64a48300ac7e09ffb31ce7b74af24e03f7672
Ajay Poshak <poshakajay@gmail.com>
2016-09-25 10:34:20 +0530

    Added function call to retrieve article by id

--- MyBlog/index.js
+++ MyBlog/index.js
@@ -1,29 +1,43 @@
 "use strict";
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-     host: 'localhost',
-     user: 'root',
-     password: 'mountain',
-     database : 'blog'
-   });
-   /**
-    * This exports the functions to require call
-    * @param  {response} standard response type
-    * @return {rows|err}  return rows or err based on the database response
-    */
-module.exports = function(response){
-  return {
-    selectData: function(){
-      connection.query('SELECT * from contents', function(err, rows, fields){
-             if(!err){
-              console.log('Connection is working...', rows);
-              response.end(JSON.stringify(rows));
-              return rows;
+    host: 'localhost',
+    user: 'root',
+    password: 'mountain',
+    database: 'blog'
+});
+/**
+ * This exports the functions to require call
+ * @param  {response} standard response type
+ * @return {rows|err}  return rows or err based on the database response
+ */
+module.exports = function(response) {
+    return {
+        selectData: function() {
+            connection.query('SELECT * FROM contents ORDER BY createdon DESC LIMIT 5', function(err, rows, fields) {
+                if (!err) {
+                    console.log('Connection is working...', rows);
+                    response.end(JSON.stringify(rows));
+                    return rows;
+                } else
+                    console.log('Not working...', err);
+                return err;
+            });
+        },
+        getArticlebyId: function(articleId) {
+            if (parseInt(articleId) === parseInt(articleId)) {
+                connection.query('SELECT * FROM contents WHERE articleid=' + parseInt(articleId),
+                    function(err, rows, field) {
+                        if (!err) {
+                            console.log(rows);
+                            response.end(JSON.stringify(rows));
+                            return rows;
+                        } else {
+                            console.log("Not working...", err);
+                            return err;
+                        }
+                    });
             }
-            else
-              console.log('Not working...', err);
-              return err;
-          });
         }
-      }
-    };
+    }
+};
